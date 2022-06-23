5246b17b2d9861f533bcbf8c29d604dd61f70afc
Karypbaeva Amina <karypbaeva.amina59@gmail.com>
2022-06-24 03:17:57 +0600

    Init4

--- src/Operation.java
+++ src/Operation.java
@@ -1,9 +1,11 @@
 public enum Operation {
     ROCK,
     PAPER,
-    SCISSORS;
+    SCISSORS,
+    SPOCK,
+    LIZARD;
 
-    public static int length;
+
 
     public int searchWinner (Operation otherMove) {
         if (this == otherMove)
@@ -16,7 +18,28 @@ public enum Operation {
             case SCISSORS:
                 return (otherMove == PAPER ? 1 : -1);
         }
+        return 0;
+    }
 
+
+
+    public int searchWinner2 (Operation otherMove) {
+        if (this == otherMove)
+            return 0;
+        switch (this) {
+            case ROCK:
+                return (otherMove == SCISSORS || otherMove == LIZARD? 1 : -1);
+            case PAPER:
+                return (otherMove == ROCK || otherMove==SPOCK ? 1 : -1);
+            case SCISSORS:
+                return (otherMove == PAPER || otherMove == LIZARD ? 1 : -1);
+            case SPOCK:
+                return (otherMove == ROCK || otherMove==SCISSORS ? -1 : 1);
+            case LIZARD:
+                return (otherMove == PAPER || otherMove == ROCK ? 1 : -1);
+
+            default:
+        }
         return 0;
     }
 }
--- src/User.java
+++ src/User.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 public class User {
 
-
     public User() {
 
     }
@@ -21,9 +20,40 @@ public class User {
                     return Operation.PAPER;
                 case 'S':
                     return Operation.SCISSORS;
+
             }
         }
         return getUser();
     }
+
+    public static Operation getUser2() {
+        System.out.println(" Enter one of these actions ROCK, SCISSORS, PAPER, SPOCK, LIZARD");
+        Scanner inputScanner = new Scanner(System.in);
+        String userChoose =inputScanner.nextLine();
+        userChoose = userChoose.toUpperCase();
+        char firstLetter = userChoose.charAt(0);
+        if (firstLetter == 'R' || firstLetter == 'P' || firstLetter == 'S' || firstLetter == 'S' || firstLetter == 'L') {
+            switch (firstLetter) {
+                case 'R':
+                    return  Operation.ROCK;
+                case 'P':
+                    return Operation.PAPER;
+                case 'S':
+                    return Operation.SCISSORS;
+                case 'S':
+                    return Operation.SCISSORS;
+                case 'L':
+                    return Operation.SCISSORS;
+
+
+
+
+            }
+        }
+        return getUser();
+    }
+
+
+
 
 }
