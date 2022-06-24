d1f181eccea39d526178707a716e8841d90577f2
Karypbaeva Amina <karypbaeva.amina59@gmail.com>
2022-06-24 15:24:42 +0600

    final

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Game game = new Game();
-        game.startDifficultGame();
+        System.out.println( "Choose a game 1-simple, 2-difficult");
+        Scanner scanner= new Scanner(System.in);
+        int num = scanner.nextInt();
+        if (num==1){
+            game.startGame();
+        } else if (num ==2) {
+            game.startDifficultGame();
+        } else {
+            System.out.println("Incorrect number ");
+        }
     }
 }
